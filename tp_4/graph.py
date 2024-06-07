1de46ad0aa1e5389035a0e9e7426cdbf6f30e9e1
ilan nomberg <ilunomberg@gmail.com>
2024-06-07 17:10:56 -0300

    seco

--- tp_4/graph.py
+++ tp_4/graph.py
@@ -1,4 +1,4 @@
-from typing import Optional, Any, List
+from typing import Optional, Any, List, Dict, Set
 
 
 class Graph:
@@ -7,6 +7,12 @@ class Graph:
     """
     def __init__(self):
         self._graph = {}
+        self._index = 0
+        self._stack = []
+        self._indices = {}
+        self._low_links = {}
+        self._on_stack = set()
+        self._sccs = []
 
     def add_vertex(self, vertex: str, data: Optional[Any]=None) -> None:
         """
@@ -27,7 +33,7 @@ class Graph:
         if not vertex1 in self._graph or not vertex2 in self._graph:
             raise ValueError("The vertexes do not exist")
         self._graph[vertex1]['neighbors'][vertex2] = data
-        self._graph[vertex2]['neighbors'][vertex1] = data
+        # self._graph[vertex2]['neighbors'][vertex1] = data # Uncomment this line to make the graph undirected
 
     def get_neighbors(self, vertex) -> List[str]:
         """
@@ -88,3 +94,77 @@ class Graph:
         :return: boolean
         """
         return vertex1 in self._graph and vertex2 in self._graph[vertex1]['neighbors']
+    
+    def _strong_connect(self, vertex: str) -> None:
+        """
+        Non-recursive helper function for Tarjan's algorithm to find strongly connected components
+        """
+        stack = [(vertex, 0)]
+        visited = set()
+        
+        while stack:
+            v, index = stack[-1]
+            
+            if v not in visited:
+                visited.add(v)
+                self._indices[v] = self._index
+                self._low_links[v] = self._index
+                self._index += 1
+                self._stack.append(v)
+                self._on_stack.add(v)
+            
+            neighbors = self.get_neighbors(v)
+            
+            if index < len(neighbors):
+                neighbor = neighbors[index]
+                stack[-1] = (v, index + 1)
+                
+                if neighbor not in self._indices:
+                    stack.append((neighbor, 0))
+                elif neighbor in self._on_stack:
+                    self._low_links[v] = min(self._low_links[v], self._indices[neighbor])
+            else:
+                if self._low_links[v] == self._indices[v]:
+                    scc = set()
+                    while True:
+                        w = self._stack.pop()
+                        self._on_stack.remove(w)
+                        scc.add(w)
+                        if w == v:
+                            break
+                    self._sccs.append(scc)
+                stack.pop()
+                if stack:
+                    w, _ = stack[-1]
+                    self._low_links[w] = min(self._low_links[w], self._low_links[v])
+
+    def find_strongly_connected_components(self) -> List[Set[str]]:
+        """
+        Finds and returns all strongly connected components
+        """
+        self._index = 0
+        self._stack = []
+        self._indices = {}
+        self._low_links = {}
+        self._on_stack = set()
+        self._sccs = []
+
+        for vertex in self._graph:
+            if vertex not in self._indices:
+                self._strong_connect(vertex)
+
+        return self._sccs
+
+    def largest_strongly_connected_component(self) -> int:
+        """
+        Returns the size of the largest strongly connected component
+        """
+        sccs = self.find_strongly_connected_components()
+        return max(len(scc) for scc in sccs) if sccs else 0
+
+    def number_of_strongly_connected_components(self) -> int:
+        """
+        Returns the number of strongly connected components
+        """
+        sccs = self.find_strongly_connected_components()
+        return len(sccs)
--- tp_4/tp4.py
+++ tp_4/tp4.py
@@ -2,7 +2,7 @@ from graph import Graph
 
 page_graph = Graph()
 
-with open('web-Google.txt', 'r') as file:
+with open('tp_4\web-Google.txt', 'r') as file:
     for l in file:
         if "# FromNodeId	ToNodeId" in l:
             break
@@ -14,3 +14,10 @@ with open('web-Google.txt', 'r') as file:
             if not page_graph.vertex_exists(v):
                 page_graph.add_vertex(str(v))
         page_graph.add_edge(str(edge[0]), str(edge[1]))
+
+# Calcular y mostrar los resultados
+largest_scc_size = page_graph.largest_strongly_connected_component()
+num_scc = page_graph.number_of_strongly_connected_components()
+
+print("Tamaño de la componente fuertemente conexa más grande:", largest_scc_size)
+print("Número total de componentes fuertemente conexas:", num_scc)
